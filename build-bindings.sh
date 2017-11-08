b8b16c39df31123c50aa2e3c7aafe70fe5da22fc
Gerhard Roselt <g.roselt@winton.com>
2017-11-08 22:45:40 +0000

    Add initial script to build C# implementation

---
+++ build-bindings.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pushd bindings/cs/Protean-cs/Protean.Net/
+dotnet build -f netstandard1.3
+dotnet build -f netstandard2.0
+popd
+
+pushd bindings/cs/Protean-cs/Protean.Net.Tests/
+#dotnet test -f netcoreapp1.0 Protean.Net.Tests.csproj
+dotnet test -f netcoreapp2.0 Protean.Net.Tests.csproj
+popd
