7f177c82269ebcc9f481d774aedf41836dd298bc
Gerhard Roselt <g.roselt@winton.com>
2017-11-13 22:09:48 +0000

    Use predefined lists

--- build-bindings.sh
+++ build-bindings.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+frameworks=(netcoreapp1.0 netcoreapp2.0)
+configurations=(Debug Release)
+
 pushd bindings/cs/Protean-cs/Protean.Net.Tests/
 
-for framework in netcoreapp1.0 netcoreapp2.0
+for framework in ${frameworks[@]}
 do
-  for config in Debug Release
+  for config in ${configurations[@]}
   do
     dotnet test -f $framework Protean.Net.Tests.csproj -c $config
   done
