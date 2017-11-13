c932747ae09c3d0cfe67082bcb9149ba238ddec4
Gerhard Roselt <g.roselt@winton.com>
2017-11-13 21:27:56 +0000

    Test netcoreapp1.0

--- build-bindings.sh
+++ build-bindings.sh
@@ -6,6 +6,6 @@ dotnet build -f netstandard2.0
 popd
 
 pushd bindings/cs/Protean-cs/Protean.Net.Tests/
-#dotnet test -f netcoreapp1.0 Protean.Net.Tests.csproj
+dotnet test -f netcoreapp1.0 Protean.Net.Tests.csproj
 dotnet test -f netcoreapp2.0 Protean.Net.Tests.csproj
 popd
