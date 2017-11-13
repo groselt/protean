af3b7fd4baab147a9d366f9501c2d6b7a40f0cb3
Gerhard Roselt <g.roselt@winton.com>
2017-11-13 21:52:36 +0000

    Test Debug and Release

--- build-bindings.sh
+++ build-bindings.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-pushd bindings/cs/Protean-cs/Protean.Net/
-dotnet build -f netstandard1.3
-dotnet build -f netstandard2.0
-popd
-
 pushd bindings/cs/Protean-cs/Protean.Net.Tests/
-dotnet test -f netcoreapp1.0 Protean.Net.Tests.csproj
-dotnet test -f netcoreapp2.0 Protean.Net.Tests.csproj
+dotnet test -f netcoreapp1.0 Protean.Net.Tests.csproj -c Debug
+dotnet test -f netcoreapp1.0 Protean.Net.Tests.csproj -c Release
+
+dotnet test -f netcoreapp2.0 Protean.Net.Tests.csproj -c Debug
+dotnet test -f netcoreapp2.0 Protean.Net.Tests.csproj -c Release
 popd
